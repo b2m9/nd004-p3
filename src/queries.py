a50489ce549f9dcd9f7bff39baf32a03eab6a0ee
Bob Massarczyk <bob@b2m9.com>
2017-05-04 16:31:00 +0200

    Refactor 3rd query
    - No post processing in Python needed

--- src/queries.py
+++ src/queries.py
@@ -1,3 +1,32 @@
+# ## QUERY FOR FIRST QUESTION
+# Select top 3 articles
+TOP_ARTICLES = """
+    select title, visits
+        from visits_per_article
+        order by visits desc
+        limit 3;
+"""
+
+# ## QUERY FOR SECOND QUESTION
+# Select authors sorted by visits of their respective articles
+TOP_AUTHORS = """
+    select authors.name, sum(visits_per_article.visits) as total
+        from authors, visits_per_article
+            where authors.id = visits_per_article.author_id
+            group by authors.name
+            order by total desc;
+"""
+# ## QUERY FOR THIRD QUESTION
+# Select all dates with an error rate of more than 1%
+# Less readable query but no post-processing needed
+ERROR_DATES = """
+select date, trunc(ratio::NUMERIC * 100, 2) from 
+    (select to_char(day, 'DD Mon YYYY') as date,
+        (status_not_ok::float / status_ok::float) as ratio 
+            from status_per_day_in_july) as sub
+    where ratio >= 0.01;
+"""
+
 """
 1.) What are the most popular 3 articles of all time?
 
@@ -9,10 +38,7 @@ create materialized view visits_per_article as
             where path = ('/article/' || articles.slug)
             group by title, author;
 
-select title, visits
-    from visits_per_article
-    order by visits desc
-    limit 3;
+
 
 
 
