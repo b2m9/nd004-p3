535c17da734583a558c8a092013ea45aef8376e4
Bob Massarczyk <bob@b2m9.com>
2017-05-04 15:10:41 +0200

    refined queries

--- queries.py
+++ queries.py
@@ -28,14 +28,21 @@ select authors.name, sum(visits_per_article.visits) as total
 
 3.) On which days did more than 1% of requests lead to errors?
 
-create materialized view status_per_day as
-    select to_char(time, 'YYYY-MM-DD') as day,
+create materialized view status_per_day_in_july as
+    select date_trunc('day', time) as day,
             count(*) filter (where status = '200 OK') as status_ok,
             count(*) filter (where status != '200 OK') as status_not_ok
                 from log
                     group by day;
 
-select *
-    from status_per_day
+select to_char(day, 'DD Mon YYYY') as date,
+    status_ok as ok, status_not_ok as not_ok
+    from status_per_day_in_july
     where status_ok * 0.01 <= status_not_ok;
+
+select * from 
+    (select to_char(day, 'DD Mon YYYY') as date,
+        (status_not_ok::float / status_ok::float) as ratio 
+            from status_per_day_in_july) as sub
+    where ratio >= 0.01;
 """
