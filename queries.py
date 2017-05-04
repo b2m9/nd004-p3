994442a01f3c61d4b19385d3bc5afe6aaf84433d
Bob Massarczyk <bob@b2m9.com>
2017-05-04 13:24:17 +0200

    Answered second question

--- queries.py
+++ queries.py
@@ -1,10 +1,25 @@
 """
 1.) What are the most popular 3 articles of all time?
 
-select articles.title, count(*) as c
+select articles.title, count(*) as visits
     from log, articles
     where path = ('/article/' || articles.slug)
     group by articles.title
-    order by c desc
+    order by visits desc
     limit 3;
+"""
+
+"""
+2.) Who are the most popular authors of all time
+
+select authors.name, sum(subq.visits) as total
+    from authors, 
+        (select articles.title, articles.author, count(*) as visits
+            from log, articles
+            where path = ('/article/' || articles.slug)
+            group by articles.title, articles.author
+            order by visits desc) as subq
+    where authors.id = subq.author
+    group by authors.name
+    order by total desc;
 """
